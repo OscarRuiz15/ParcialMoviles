d09242014c06b02cb474d4fb1f62dc5362629974
andresFMedina <andres.medina@correounivalle.edu.co>
2018-05-11 21:19:27 -0500

    Funciones agregadas

--- app/src/main/java/com/univalle/parcial/parcial/activities/MainActivity.java
+++ app/src/main/java/com/univalle/parcial/parcial/activities/MainActivity.java
@@ -1,20 +1,31 @@
 package com.univalle.parcial.parcial.activities;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.univalle.parcial.parcial.R;
-import com.univalle.parcial.parcial.fragments.RegistrarVentaFragment;
+import com.univalle.parcial.parcial.conexion.ConexionBD;
+import com.univalle.parcial.parcial.fragments.ConsultarVentasCliente;
 import com.univalle.parcial.parcial.fragments.Registro_Producto;
+import com.univalle.parcial.parcial.fragments.VerVentas;
+import com.univalle.parcial.parcial.modelo.Cliente;
 
-public class MainActivity extends AppCompatActivity implements RegistrarVentaFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements ConsultarVentasCliente.OnFragmentInteractionListener,Registro_Producto.OnFragmentInteractionListener,VerVentas.OnFragmentInteractionListener {
 
     private TextView mTextMessage;
+    Cliente cliente;
+    ConexionBD conexion;
+    SQLiteDatabase db;
+    ConsultarVentasCliente consultventcliente;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,14 +33,17 @@ public class MainActivity extends AppCompatActivity implements RegistrarVentaFra
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
+                case R.id.navigation_consultar_venta_cliente:
+                    crearFragmentConsultarCliente();
+                    return true;
+                case R.id.navigation_consultar_ventas:
+                    crearFragmentVentasTotales();
+                    return true;
+                case R.id.navigation_registrar_producto:
+                    registrarProducto();
+                    return true;
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //crearFragmentConsultarCliente();
                     return true;
             }
             return false;
@@ -39,28 +53,92 @@ public class MainActivity extends AppCompatActivity implements RegistrarVentaFra
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        crearFragmentRegistrar();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
+        /*Bundle bundle = getIntent().getExtras();
+        int id = bundle.getInt("id");
+        String nombre = bundle.getString("nombre");
+        String email = bundle.getString("email");
+        String apellido= bundle.getString("apellido");
+        cliente = new Cliente(id, nombre, apellido, email);*/
+
+        /*if (tipo == 0) {
+            crearFragmentRegistrar();
+        }*/
+
+        setContentView(R.layout.activity_main);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
-    public void crearFragmentRegistrar(){
-
-
-
-        RegistrarVentaFragment fragment = RegistrarVentaFragment.newInstance(null);
+    /* public void crearFragmentRegistrar() {
+         Toast.makeText(getApplication(), "Registrar", Toast.LENGTH_LONG).show();
+         Bundle bundle = new Bundle();
+         bundle.putInt("id", cliente.getId());
+         bundle.putString("nombre", cliente.getNombre());
+         bundle.putString("apellido", cliente.getApellido()));
+         bundle.putString("email", cliente.getEmail());
+         FragmentRegistrar fragment = FragmentRegistrar.newInstance(bundle);
+         android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+         ft.add(R.id.fragment, fragment);
+         ft.commit();
+     }
+ */
+    public void crearFragmentConsultarCliente() {
+        ConsultarVentasCliente fragment = ConsultarVentasCliente.newInstance();
         android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.add(R.id.fragmentventa,fragment);
+        ft.replace(R.id.fragment,fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void crearFragmentVentasTotales(){
+        VerVentas fragment = VerVentas.newInstance();
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment,fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
 
 
+    public void registrarProducto(){
+        Registro_Producto fragment = Registro_Producto.newInstance("","");
+
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment,fragment);
+        ft.addToBackStack(null);
         ft.commit();
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    ////////MÉTODOS NECESARIOS PARA INFLAR LA VISTA CON UN MENU//////
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate activity menu items.
+        getMenuInflater().inflate(R.menu.logout, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSalir:
+                Intent intent = new Intent(MainActivity.this, ActivityLogin.class);
+                intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP | intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                //Toast.makeText(this,"hola "+item.getTitle(),Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/com/univalle/parcial/parcial/fragments/VerVentas.java
@@ -0,0 +1,156 @@
+package com.univalle.parcial.parcial.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.univalle.parcial.parcial.R;
+import com.univalle.parcial.parcial.conexion.VentaBD;
+import com.univalle.parcial.parcial.modelo.Producto;
+import com.univalle.parcial.parcial.modelo.Venta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link VerVentas.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link VerVentas#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VerVentas extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private ArrayAdapter<String> adaptador;
+    private ListView ventasList;
+    List<Venta> ventas;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public VerVentas() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment VerVentas.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VerVentas newInstance() {
+        VerVentas fragment = new VerVentas();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_ver_ventas, container, false);
+
+        VentaBD vd=new VentaBD(getContext(), "Parcial", null, 1);
+        ventas=vd.consultarVentas(getContext());
+
+        ArrayList<String> datosVenta=new ArrayList<String>();
+        int totalVentas=0;
+
+        for (int i = 0; i < ventas.size(); i++) {
+            datosVenta.add("Venta #"+ventas.get(i).getId()+"\nCliente: "+ventas.get(i).getCliente().getNombre()+" "+ventas.get(i).getCliente().getApellido()+"\nTotal: "+ventas.get(i).getTotal()+"\nFecha: "+ventas.get(i).getFecha());
+            totalVentas+=ventas.get(i).getTotal();
+        }
+        datosVenta.add("Total Ventas: "+totalVentas);
+
+        adaptador=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,datosVenta);
+        ventasList=v.findViewById(R.id.ventasList);
+        ventasList.setAdapter(adaptador);
+
+        ventasList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String componente= ventasList.getAdapter().getItem(position).toString();
+                //Toast.makeText(getApplication(),"Hola: "+componente+" "+position, Toast.LENGTH_SHORT).show();
+
+                String nombreCliente=ventas.get(position).getCliente().getNombre()+" "+ventas.get(position).getCliente().getApellido();
+                Producto pro=ventas.get(position).getProducto();
+                String productos=""+pro.getItem();
+
+                Toast.makeText(getContext(),""+nombreCliente+"\n"+productos, Toast.LENGTH_SHORT).show();
+                /*DetallesVenta fragment = DetallesVenta.newInstance(ventas);
+                android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frameVentas,fragment);
+                ft.addToBackStack(null);
+                ft.commit();*/
+            }
+        });
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
